1a449264d72aa06167aff0cec7b144ddd7ebab2e
FrankMrg <64907047+FrankMrg@users.noreply.github.com>
2020-07-30 18:54:36 +0200

    Update paginaRicerca.component.js
    Migliorato la parte grafica di front-end di effettua ricerca

--- React-Express-Test/client/src/components/paginaRicerca.component.js
+++ React-Express-Test/client/src/components/paginaRicerca.component.js
@@ -19,19 +19,27 @@ export default class PaginaRicerca extends Component {
         console.log(this.state)
         //Crea un oggetto che contiene il mapping dei dati come componenti <li>
         const listItems = this.state.map((d) =>
-            <div key={'div' + d.idAnnuncio} style={{ marginBottom: '4rem' }}>
-                <img key={'img' + d.idAnnuncio} style={{ width: '60%', marginLeft: '20%', marginBottom: '2rem' }} src={require('../../../images/ID' + d.idAnnuncio + '/Cover.png')} alt="CoverImage"  ></img>
-                <h5 key={'li' + d.idAnnuncio}>IDAnnuncio: {d.idAnnuncio} - Citta: {d.citta} - Indirizzo: {d.indirizzo} - Num_Bagni: {d.n_bagni}</h5>
-            </div>);
+            //<li class="list-group-item" style={{ marginBottom: '4rem',  backgroundColor: 'grey'}}>
+            <a href="prova123.html" class="list-group-item list-group-item-action border border-primary " style={{ marginTop: '1rem', marginBottom: '1rem', background: '#E6E6FA'}}>
+                <div class='row' >
+                    <div class='col-6' key={'div' + d.idAnnuncio} style={{ marginTop: '1rem', marginBottom: '1rem'}}>
+                         <img key={'img' + d.idAnnuncio} style={{ width: '100%'}} src={require('../../../images/ID' + d.idAnnuncio + '/Cover.png')} alt="CoverImage"  ></img>
+                    </div>
+                    <div class='col-6' style={{ marginTop: '2rem'}}>
+                        <h5 key={'li' + d.idAnnuncio}>-ID dell'annuncio: {d.idAnnuncio}<br></br> -città: {d.citta}<br></br> -indirizzo: {d.indirizzo}<br></br> -numero bagni: {d.n_bagni}<br></br> -numero posti letto: {d.n_posti} </h5>
+                    </div>
+                </div> 
+            </a>
+            );
         return (
             <div className="container justify-content-center">
-                <div className="col-md-9 py-5 " style={{ marginLeft: '12.5%' }}>
-                    <div className="card">
+                <div className="col-md-9 py-5 " style={{ marginLeft: '12.5%'}}>
+                    <div className="card" style={{background: '#FFFACD'}}>
                         <div className="card-body" style={{ padding: '2rem' }}>
-                            <h1 className="h3" style={{ padding: '1rem', textAlign: 'center', marginBottom: '2rem' }}>Risultati Ricerca</h1>
-
-                            {listItems}
-
+                            <h1 className="h2" style={{ padding: '1rem', textAlign: 'center', marginBottom: '2rem' }}>Risultati della ricerca</h1>
+                            <div class="list-group" >
+                                {listItems}
+                            </div>
                         </div>
                     </div>
                 </div>
