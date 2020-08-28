6556e8c342cca24324f2c0c82876aab2870aa287
Nihil-01 <64413865+Nihil-01@users.noreply.github.com>
2020-08-28 17:15:47 +0200

    Fix Routing DB

--- React-Express/api/routes/gestioneLegale.js
+++ React-Express/api/routes/gestioneLegale.js
@@ -25,9 +25,7 @@ async function invioDatiQuestura(req, res, next) {
 
     console.log(req.body.dati.nomeCognome)
 
-    res.status(200).send('Invio riuscito')
-
-    /*const db = await makeDb(config);
+    const db = await makeDb(config);
     let results = {};
 
     try {
@@ -58,13 +56,12 @@ async function invioDatiQuestura(req, res, next) {
 
     var textEmail
     var dati = req.body.dati
-    for(let i=0; i<dati.nomeCognome.lenght;i++)
-    {
-        textEmail += 'Ospite '+ i + ': ' + dati.nomeCognome[i]
+    for (let i = 0; i < dati.nomeCognome.lenght; i++) {
+        textEmail += 'Ospite ' + i + ': ' + dati.nomeCognome[i] + '\nCodice Fiscale: ' + dati.codiceFiscale[i] + '\n\n'
     }
 
     var mailOptions = {
-        attachments: [req.body.dati.formData.file]
+        attachments: [req.body.dati.formData.file],
         from: 'teammars44@gmail.com',
         to: 'emailQuestura@questura.it',
         subject: 'Dati Ospiti',
@@ -75,8 +72,9 @@ async function invioDatiQuestura(req, res, next) {
             console.log(error);
         } else {
             console.log('Email inviata: ' + info.response);
+            res.status(200).send('Invio riuscito')
         }
-    });*/
+    });
 }
 
 
